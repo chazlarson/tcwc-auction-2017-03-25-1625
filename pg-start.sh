b55b6f1f946a46e665f0ad386708b4169e8cd86f
Chaz Larson <chaz@chazlarson.com>
2017-03-26 02:40:33 +0000

    Added pg-start.sh

---
+++ pg-start.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# NOTE: This step is necessary if you are using PostgreSQL in a Docker development environment.
+
+echo '-----------------------------'
+echo 'sudo service postgresql start'
+sudo service postgresql start
+
